c6317060ca68bfcd638cfec4b9c0bd5463d951a4
austin <posty65841@gmail.com>
2022-09-30 17:58:37 -0500

    alomst done need to fix some routing issues

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require("express")
+const express = require("express");
+const { readFromFile, readAndAppend, readAndDelete } = require('./helpers/fsUtils');
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const db = require("./db/db.json")
+const db = require("./db/db.json");
+const uuid = require('./helpers/uuid');
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
@@ -12,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
-app.get("/notes", (req, res) =>{
+app.get("/notes", (req, res) => {
 
 
     res.sendFile(path.join(__dirname, "/public/notes.html"));
@@ -20,13 +22,44 @@ app.get("/notes", (req, res) =>{
 
 })
 
-app.get("/api/notes", (req, res) =>{
+app.get("/api/notes", (req, res) => {
 
-
-
-
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 
 
 })
 
-app.listen(PORT, ()=> console.log("listening on port http://localhost:"+PORT))
+
+
+app.post("/api/notes", function (req, res) {
+    console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+     title, 
+     text,
+     id: uuid()
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+
+
+app.delete("/api/notes/:id", function (req, res) {
+
+
+
+
+    readAndDelete('./db/db.json', req.params.id);
+    res.json(`Note deleted successfully 🚀`);
+
+});
+
+app.listen(PORT, () => console.log("listening on port http://localhost:" + PORT))
